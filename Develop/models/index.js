cd2d05f93c77a6fd47a4670b232eabf34e43c51c
blancahidalgo <b.hidalgo.ley@gmail.com>
2023-05-14 18:59:58 +1000

    fixed my model index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,38 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_category'
-});
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+  });
 
 // Categories have many Products
-Category.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'category_products'
-});
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+  });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_tags'
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'tag_products'
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
